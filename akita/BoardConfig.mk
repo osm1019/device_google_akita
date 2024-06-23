078863bf5c5f6d56ddea3cd712d39259b1832043
AnierinB <anierin@evolution-x.org>
2024-06-23 04:14:44 +0000

    akita: Initialize for Evolution X 9.x
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- akita/BoardConfig.mk
+++ akita/BoardConfig.mk
@@ -47,3 +47,4 @@ include device/google/akita/sepolicy/akita-sepolicy.mk
 include device/google/akita/wifi/BoardConfig-wifi.mk
 
 include device/google/akita/akita/BoardConfigLineage.mk
+include device/google/akita/akita/BoardConfigEvolution.mk
